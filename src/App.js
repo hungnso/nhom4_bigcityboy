22a88fd19af2ea68429f763d86eb1b2e667e16d6
dqtien2810 <quangtiendinh2808@gmail.com>
2022-01-13 17:02:07 +0700

    tiendq code home side bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import GroupForm from "./GroupForm";
 // import Mapbox from "./MapAddAddress/mapbox"
 import AnnouncingVote from "./AnnouncingVote/announcingVote"
+import Home from "./HomeSidebar"
 function App() {
   // return <GroupForm />;
   return (
     <div className="App">
       
       {/* <Mapbox /> */}
-      <AnnouncingVote />
+      {/* <AnnouncingVote /> */}
+      <Home></Home>
     </div>
   );
 }
