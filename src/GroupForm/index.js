5a594d0be25c9637dc706269ec8c06c68722698b
NVKhanh <nvk020697@gmail.com>
2022-01-13 10:59:25 +0700

    up code component modalForm

--- src/GroupForm/index.js
+++ src/GroupForm/index.js
@@ -1,18 +1,11 @@
-import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Modal,
-  ModalBody,
-  ModalHeader,
-  ModalFooter,
-} from "reactstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import InputForm from "../components/InputForm";
+import ModalForm from "../components/ModalForm";
 
 function GroupForm() {
+  const [show, setShow] = useState(false);
   return (
     <div className="login_form">
       <div className="krqetT"></div>
@@ -40,30 +33,16 @@ function GroupForm() {
               </div>
 
               <div className="address_vote">
-                <Button color="danger" onClick={function noRefCheck() {}}>
-                  Công viên Hòa Bình
+                <Button onClick={() => setShow(true)}>
+                  số 2 Hùng Vương, Điện Bàn, Ba Đình, Hà Nội
                 </Button>
-                <Modal toggle={function noRefCheck() {}}>
-                  <ModalHeader toggle={function noRefCheck() {}}>
-                    Modal title
-                  </ModalHeader>
-                  <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button color="primary" onClick={function noRefCheck() {}}>
-                      Do Something
-                    </Button>{" "}
-                    <Button onClick={function noRefCheck() {}}>Cancel</Button>
-                  </ModalFooter>
-                </Modal>
+                <ModalForm
+                  show={show}
+                  onHide={() => setShow(false)}
+                  ModalTile={""}
+                  ModalChildren={"map"}
+                  size={"lg"}
+                />
               </div>
 
               <div className="login_btn_wrapper">
