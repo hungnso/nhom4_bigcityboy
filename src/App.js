5f2524e12d223eab3c03a0e796edc48d9f1c11fe
nam <donam3005@gmail.com>
2022-01-13 10:42:17 +0700

    up code properties MapAddAddress and AnnouncingVote

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import GroupForm from "./GroupForm";
-
+// import GroupForm from "./GroupForm";
+// import Mapbox from "./MapAddAddress/mapbox"
+import AnnouncingVote from "./AnnouncingVote/announcingVote"
 function App() {
-  return <GroupForm />;
+  // return <GroupForm />;
+  return (
+    <div className="App">
+      
+      {/* <Mapbox /> */}
+      <AnnouncingVote />
+    </div>
+  );
 }
 
 export default App;
