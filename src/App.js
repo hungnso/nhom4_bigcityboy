8c24fc161d5eef0e9dc405f90edf7296a64b0489
nam <donam3005@gmail.com>
2022-01-14 10:12:34 +0700

    upcode AnnuoncingVote

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import GroupForm from "./GroupForm";
-// import Mapbox from "./MapAddAddress/mapbox"
-import AnnouncingVote from "./AnnouncingVote/announcingVote"
+import GroupForm from "./GroupForm";
+// import LoginFrom from "./LoginForm";
+import Mapbox from "./MapAddAddress/mapbox"
+// import AnnouncingVote from "./AnnouncingVote/announcingVote"
 function App() {
-  // return <GroupForm />;
-  return (
-    <div className="App">
+  return <GroupForm />;
+ 
+  // return (
+  //   <div className="App">
       
-      {/* <Mapbox /> */}
-      <AnnouncingVote />
-    </div>
-  );
+  //     <Mapbox />
+  //     {/* <AnnouncingVote /> */}
+  //   </div>
+  // );
 }
 
 export default App;
