d4a1715951a27fd50dc581f243105e96c7df0ab8
dqtien2810 <quangtiendinh2808@gmail.com>
2022-01-17 16:46:06 +0700

    update-home

--- src/HomeSidebar/homeSidebar.js
+++ src/HomeSidebar/homeSidebar.js
@@ -4,6 +4,8 @@ import './homeSidebar.css'
 import { useNavigate } from 'react-router-dom'
 import ModalForm from '../components/ModalForm'
 import PopupForm from '../components/PopupForm'
+
+
 const HomeSidebar = () => {
   const navigate = useNavigate()
   const [show, setShow] = useState(false)
@@ -12,9 +14,11 @@ const HomeSidebar = () => {
     e.preventDefault()
     navigate('/announcingVote')
   }
+
   return (
     <div className="home">
       <div className="home-sidebar">
+        <button class="go-back"><span>Quay lại</span></button> 
         <div className="home-sidebar-title">
           <h2>Title here</h2>
         </div>
@@ -22,27 +26,47 @@ const HomeSidebar = () => {
           <h2>Content here</h2>
         </div>
         <div className="home-sidebar-members">
-          <div className="vote">
-            <h4 className="nameVote">Công Viên Yên Sở</h4> <h5 className="quantilyVote">10</h5>
+        <div className="vote">
+            <h4 className="nameVote">
+                <input type="checkbox"></input>
+                Công Viên Yên Sở
+            </h4>
+            <h5 className="quantilyVote">10</h5>
           </div>
           <div className="vote">
-            <h4 className="nameVote">Công Viên Hòa Bình</h4> <h5 className="quantilyVote">10</h5>
+            <h4 className="nameVote">
+                <input type="checkbox"></input>
+                Công Viên Hòa Bình
+            </h4>
+            <h5 className="quantilyVote">10</h5>
           </div>
           <div className="vote">
-            <h4 className="nameVote">Công Viên Thống Nhất</h4> <h5 className="quantilyVote">10</h5>
+            <h4 className="nameVote">
+                <input type="checkbox"></input>
+                Công Viên Thống Nhất
+            </h4>
+            <h5 className="quantilyVote">10</h5>
           </div>
           <div className="vote">
-            <h4 className="nameVote">Công Viên Nghĩa Đô</h4> <h5 className="quantilyVote">10</h5>
+            
+            <h4 className="nameVote">
+            <input type="checkbox"></input>Công Viên Nghĩa Đô
+            </h4>
+            <h5 className="quantilyVote">10</h5>
           </div>
           <div className="vote">
-            <h4 className="nameVote">Công Viên Thủ Lệ</h4> <h5 className="quantilyVote">5</h5>
+            <h4 className="nameVote">
+                <input type="checkbox"></input>
+                Công Viên Hòa Bình
+            </h4>
+            <h5 className="quantilyVote">10</h5>
           </div>
         </div>
         {/* <div className="home-sidebar-location">
                     
                 </div> */}
 
-        <div className="btnEndVote">
+        <div className="btnLocation_share">
           <button style={{ width: '95%' }} onClick={() => setShow(true)}>
             Địa Chỉ
           </button>
@@ -54,6 +78,20 @@ const HomeSidebar = () => {
             size="md"
           />
         </div>
+
+        <div className="btnLocation_share">
+          <button style={{ width: '95%' }} onClick={() => setShow(true)}>
+            Chia sẻ 
+          </button>
+          <ModalForm
+            show={show}
+            onHide={() => setShow(false)}
+            ModalTile={''}
+            ModalChildren={<PopupForm value={window.location.href} />}
+            size="md"
+          />
+        </div>
+
         <div className="btnEndVote">
           <button type="submit" onClick={e => handleCLick(e)}>
             END VOTE
